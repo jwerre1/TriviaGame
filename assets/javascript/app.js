5bb8c832eca2949349ed6574e9c38aa1bc23b6e2
jordan.werre <47542013+jwerre1@users.noreply.github.com>
2019-03-06 05:34:56 -0500

    players choices now being registered. game play beginning to be established

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,6 +24,9 @@ $(document).ready(function() {
     var time = 0;
     var clockRunning = false;
     var intervalID;
+    
+    var correct = 0;
+    var wrong = 0;
 
     //examples of how to reference questions/choices/answers
     // console.log(questList.length);
@@ -34,6 +37,7 @@ $(document).ready(function() {
     function promptQuestion() {
 
         $("#answer").html("");
+        // $("$answer").removeAttr("type");
 
         // sets the timer for each questions
         $("#timer").text("00:30");
@@ -48,9 +52,9 @@ $(document).ready(function() {
                 var choice = $("<button>");
                 choice.addClass("btn btn-light");
                 choice.attr("id","possibleChoice");
-                choice.attr("answerCheck", [i]);
+                choice.attr("answer-check", [i]);
                 choice.text(questList[questIndex].answers[i]);
-                $("#answer").append(choice);
+                $("#choice" + i).append(choice);
             }
         }
 
@@ -85,6 +89,34 @@ $(document).ready(function() {
 
         }
     }
+
+
+    function nextQuest () {
+        if (questIndex <= (questList.length - 1)) {
+            promptQuestion();
+        }
+        else {
+            $("#question").text("Game Over!");
+            $("#answer").empty();
+            $("#choice0").text("Correct: " + correct);
+            $("#choice1").text("Incorrect: " + wrong);
+
+        }
+
+
+
+    // if (questionIndex <= (questions.length - 1)) {
+    //     document.querySelector("#question").innerHTML = questions[questionIndex].q;
+    //   }
+    //   // If there aren't, render the end game screen.
+    //   else {
+    //     document.querySelector("#question").innerHTML = "Game Over!";
+    //     document.querySelector("#score").innerHTML = "Final Score: " + score + " out of " + questions.length;
+    //   }
+    // }
+    }
+
+
     //function to call when starting timer
     function timerStart() {
         time = 30;
@@ -134,21 +166,107 @@ $(document).ready(function() {
         promptQuestion();
     });
 
+    for (i = 0; i < 4; i++) {
+        $("#choice" + [i]).attr("answer-check", [i]);
+    }
+
     // $("#answer").click(checkAnswer);
 
-    $("#answer").on("click", function() {
-        console.log("what");
-        var chosenAnswer = ($(this).attr("answerCheck"));
+    console.log(questList[questIndex].correctAnswer);
+
+    $("#choice0").on("click", function() {
+        console.log("what choice3");
+        stop();
+        var chosenAnswer = ($(this).attr("answer-check"));
         console.log(chosenAnswer);
-        if (chosenAnswer === questList[questIndex].correctAnswer) {
+        if (chosenAnswer == questList[questIndex].correctAnswer) {
             $("#question").text("Correct!");
-
-            $("#answer").remove(choice);
-            var currentGif = $("<img>");
-            curentGif.attr("src", gifList[questIndex]);
-            $("#answer").append(currentGif);
-
+            correct++;
         }
+        else {
+            $("#question").text("Nope!");
+            wrong++;
+        }
+        for (i = 0; i < 4; i++) {
+            $("#choice" + [i]).empty();
+        }
+         
+        var currentGif = $("<img>");
+        currentGif.attr({"src":gifList[questIndex], "width":"200px"});
+        $("#answer").append(currentGif);
+
+        questIndex++;
+        setTimeout(nextQuest, 5*1000);
+        
+
+    });
+
+    $("#choice1").on("click", function() {
+        console.log("what choice3");
+        stop();
+        var chosenAnswer = ($(this).attr("answer-check"));
+        console.log(chosenAnswer);
+        if (chosenAnswer == questList[questIndex].correctAnswer) {
+            $("#question").text("Correct!");
+            correct++;
+        }
+        else {
+            $("#question").text("Nope!");
+            wrong--;
+        }
+        for (i = 0; i < 4; i++) {
+            $("#choice" + [i]).empty();
+        }
+         
+        var currentGif = $("<img>");
+        currentGif.attr({"src":gifList[questIndex], "width":"200px"});
+        $("#answer").append(currentGif);
+
+        
+
+    });
+    $("#choice2").on("click", function() {
+        console.log("what choice3");
+        stop();
+        var chosenAnswer = ($(this).attr("answer-check"));
+        console.log(chosenAnswer);
+        if (chosenAnswer == questList[questIndex].correctAnswer) {
+            $("#question").text("Correct!");
+            correct++;
+        }
+        else {
+            $("#question").text("Nope!");
+            wrong--;
+        }
+        for (i = 0; i < 4; i++) {
+            $("#choice" + [i]).empty();
+        }
+         
+        var currentGif = $("<img>");
+        currentGif.attr({"src":gifList[questIndex], "width":"200px"});
+        $("#answer").append(currentGif);
+
+        
+
+    });
+    $("#choice3").on("click", function() {
+        console.log("what choice3");
+        stop();
+        var chosenAnswer = ($(this).attr("answer-check"));
+        console.log(chosenAnswer);
+        if (chosenAnswer == questList[questIndex].correctAnswer) {
+            $("#question").text("Correct!");
+            correct++;
+            console.log(correct);
+        }
+        for (i = 0; i < 4; i++) {
+            $("#choice" + [i]).empty();
+            wrong--;
+        }
+         
+        var currentGif = $("<img>");
+        currentGif.attr({"src":gifList[questIndex], "width":"200px"});
+        $("#answer").append(currentGif);
 
     });
 
